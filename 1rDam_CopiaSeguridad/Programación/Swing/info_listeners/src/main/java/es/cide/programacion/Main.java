41bd797d8cf051946df2e33b0d2b20758a317a2c
Hugo <hcorreoso@nigulcide.es>
2026-02-09 20:33:29 +0100

    09/02/2026

---
+++ 1rDam_CopiaSeguridad/Programación/Swing/info_listeners/src/main/java/es/cide/programacion/Main.java
@@ -0,0 +1,58 @@
+package es.cide.programacion;
+
+public class Main {
+    public static void main(String[] args) {
+//         🖱️ LISTENERS DE ACCIONES
+// ActionListener
+
+// 👉 El más usado
+
+// Se activa con botones, menús, Enter en un JTextField
+
+// Método: actionPerformed(ActionEvent e)
+
+// 📌 Ejemplo: clic en un botón
+
+// ⌨️ LISTENERS DE TECLADO
+// KeyListener
+
+// Detecta teclas del teclado
+
+// keyPressed
+
+// keyReleased
+
+// keyTyped
+
+// 📌 Para juegos o atajos simples
+
+// 🖱️ LISTENERS DEL MOUSE
+// MouseListener
+
+// Eventos básicos del mouse
+
+// click
+
+// press
+
+// release
+
+// enter
+
+// exit
+
+// MouseMotionListener
+
+// Movimiento del mouse
+
+// mouseMoved
+
+// mouseDragged
+
+// MouseWheelListener
+
+// Rueda del mouse
+
+// scroll arriba / abajo
+    }
+}
